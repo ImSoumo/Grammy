6bb440c50214ebab75fd994d5beea5b076390b65
Sᴏᴜᴍᴏ <134998475+ImSoumo@users.noreply.github.com>
2024-11-04 09:15:46 +0530

    good !

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ class Pyrogram:
         api_id=29400566,
         api_hash="8fd30dc496aea7c14cf675f59b74ec6f",
         session_string="BQHAnfYAlviETjEp77nFPQv1Wc6qOZYasRnPXWLXZypy6XOXHlXKwhLcLK7wEtx-LAKQeDmvQnlLB4hKsH7Jva1I9ZJje0s1-aI9irIJc09Ctl1a2mDoMc9U2ndlGfa0XJvSV7NajQx-j5buv5Auqqn0Mj_FevtqbLcbL6kRDotmHiqkcx3due2Uk8geGNTGbe_xU9pYDD9FX4qx9gbNthevEeT4DCeR14ezSaKdJ5-mG7O7vsxaPS_oGbrJa_svPAv5muxMCmm_qvpxc_srTiPkgHGQofmyL5YS5kwwPtb81F6CyT4O_SBMDvMS2sQ-Xmuhtwd_5AZNSlN5z7HAaKe5PBmCFAAAAAGAI4AQAA",
-        plugins=dict(root="Packages")
+        plugins=dict(root="Packages"),
         in_memory=True
     )
 app = Pyrogram.app
